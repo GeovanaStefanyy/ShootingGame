7adee4b0336bb58c7fe8d9ec5c2944927b9f51d9
Geovana Stefany <157837369+GeovanaStefanyy@users.noreply.github.com>
2025-07-01 22:02:44 -0300

    Hit box collisions HUD Score

--- code/game.py
+++ code/game.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGTH, MENU_OPTION
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.level import Level
 from code.menu import Menu
 
@@ -10,7 +10,7 @@ from code.menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGTH))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
